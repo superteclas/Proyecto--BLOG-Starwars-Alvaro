7d6f85b6bbe7c93e0f9e22f03ae80c72ce9060f6
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-03-25 18:26:48 +0100

    arreglados diseños card y titulos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light mb-3" style={{width: '100%'}}> 
 			<img src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo-1.png" alt="Logo" className="navbar-brand ml-0 h1" style={{ width: '7%', marginLeft: '7em' }} />
 			<div className="ml-auto">
-				<Link to="/demo">
+				<Link to="">
 					<button className="btn btn-primary" style={{ marginRight: '8em' }}>Favorites</button>
 				</Link>
 			</div>
