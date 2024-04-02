4a3eea56c28b0654485ecda2659fc3fd092b8e46
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-04-02 13:08:01 +0200

    añadida imagen ejemplod

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -21,11 +21,16 @@ export const Single = () => {
         <div className="jumbotron">
         <div className="row align-items-center">
             <div className="col-md-3">
-                <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" alt="luke" className="img-fluid" />
+                <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" alt="luke" className="img-fluid ml-5" />
             </div>
             <div className="col-md-9">
                 <h1 className="display-4">{character.name}</h1>
-                <p className="lead">Descripción del personaje...</p>
+                <p className="lead">Sed imperdiet molestie sem vitae semper. Maecenas condimentum mattis hendrerit. 
+                Sed ultrices ante nisi, eu lobortis leo dictum vel. Mauris vel enim condimentum justo hendrerit placerat eget et erat. 
+                Integer mi justo, ultricies quis commodo ut, interdum sed ligula. Nunc sodales, tellus quis pharetra finibus, sem ante facilisis ex, ac consectetur erat est sed ipsum. 
+                Aenean at dolor quis enim efficitur pellentesque. Pellentesque luctus mi eu lacus vehicula, eget volutpat arcu maximus. 
+                Donec non nibh risus. Maecenas congue urna elit, quis tincidunt justo luctus at. 
+                Nulla ultrices bibendum consequat. Aenean quis eros ex.</p>
             </div>
         </div>
     </div>
