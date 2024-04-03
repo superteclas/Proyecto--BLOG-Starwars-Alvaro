a5af6bc008a0aae36b40a59e97dd493b58bf78c3
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-04-03 12:33:46 +0200

    añadidos layouts

--- src/js/layout.js
+++ src/js/layout.js
@@ -25,6 +25,8 @@ const Layout = () => {
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/singleplanetas/:theid" element={<Single />} />
+						<Route path="/singlevehiculos/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
