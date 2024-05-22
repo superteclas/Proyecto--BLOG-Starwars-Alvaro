b131f6595b6c5a15eba12649e095a596f106bd51
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-22 20:25:59 +0000

    añadido boton en navbar condicional

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -36,25 +36,27 @@ export const Navbar = () => {
             <Link to="/" className="navbar-brand ml-0 h1" style={{ width: '7%', marginLeft: '7em' }}>
                 <img src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo-1.png" alt="Logo" style={{ width: '100%', height: 'auto', filter: 'invert(100%)' }} />
             </Link>
-            <div className="dropdown" style={{ marginRight: '1em' }}>
-                <a className="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={toggleDropdown}>
-                    Favoritos ({favorites.length})
-                </a>
-                <ul className={"dropdown-menu" + (dropdownOpen ? " show" : "")} onClick={(e) => e.stopPropagation()}>
-                    <li className="dropdown-header">Favoritos</li>
-                    {favorites.map((favorite, index) => (
-                        <li key={index} className="d-flex align-items-center">
-                            <span className="dropdown-item mr-auto">{favorite}</span>
-                            <i className="fa fa-trash ml-2 text-black" onClick={(event) => handleRemoveFavorite(favorite, event)}></i>
-                        </li>
-                    ))}
-                    {favorites.length === 0 && (
-                        <li>
-                            <span className="dropdown-item">No hay favoritos</span>
-                        </li>
-                    )}
-                </ul>
-            </div>
+            {isLoggedIn && (
+                <div className="dropdown" style={{ marginRight: '1em' }}>
+                    <a className="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={toggleDropdown}>
+                        Favoritos ({favorites.length})
+                    </a>
+                    <ul className={"dropdown-menu" + (dropdownOpen ? " show" : "")} onClick={(e) => e.stopPropagation()}>
+                        <li className="dropdown-header">Favoritos</li>
+                        {favorites.map((favorite, index) => (
+                            <li key={index} className="d-flex align-items-center">
+                                <span className="dropdown-item mr-auto">{favorite}</span>
+                                <i className="fa fa-trash ml-2 text-black" onClick={(event) => handleRemoveFavorite(favorite, event)}></i>
+                            </li>
+                        ))}
+                        {favorites.length === 0 && (
+                            <li>
+                                <span className="dropdown-item">No hay favoritos</span>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )}
             {/* Mostrar botón de cerrar sesión si el usuario está autenticado, de lo contrario, mostrar botón de inicio de sesión */}
             {isLoggedIn ? (
                 <button className="btn btn-primary ml-3" onClick={handleLogout} style={{ marginRight: '1em' }}>Cerrar sesión</button>
