74e1d5d8e6e60d8d7a257b59b85e73aebde42fae
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-22 12:34:55 +0000

    modificando logica de login, no funciona bien aun

--- src/js/views/favorites.js
+++ src/js/views/favorites.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
+
     return (
         <h1 style={{ color: "white" }}>HOLA PAGINA DE FAVORITOS</h1>
     );
